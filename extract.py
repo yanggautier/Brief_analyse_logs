bc34519c3d7eb14ee7493a648355b7007afd37eb
yanggautier <yangguole@outlook.com>
2024-06-26 14:41:59 +0200

    fine version

--- extract.py
+++ extract.py
@@ -20,10 +20,22 @@ def write_o_file(output_filename, suspect_list):
     output_file.close()
 
 
+# Get all unique user in connexion.log
+def get_all_user(log_filename, out_filename):
+    connexion_file = open(log_filename, 'r')
+    output_file = open(out_filename, 'w')
+    out_of_hour_list = set([line.split(";")[1] for line in connexion_file])
+    for user in out_of_hour_list:
+        output_file.write(user + "\n")
+
+    connexion_file.close()
+    output_file.close()
+
+
 # Detect all users who is connected other time between 8h and 19h
 def detection_out_of_hour(log_filename):
     connexion_file = open(log_filename, 'r')
-    out_of_hour_list = [line[1] for line in connexion_file if not (8 <= int(line.split(";")[2].split()[:2]) <= 19)]
+    out_of_hour_list = list(set([line.split(";")[1] for line in connexion_file if not (8 <= int(line.split(";")[2].strip().split()[1][:2]) <= 19)]))
     print(out_of_hour_list)
 
 
@@ -47,5 +59,8 @@ def write_suspect(log_filename, warning_filename, output_filename):
 
 
 if __name__ == "__main__":
-    detection_out_of_hour("connexion.log")
-    write_suspect("connexion.log", "warning.txt", "suspect.txt")
+    input_file = "connexion.log"
+    detection_out_of_hour(input_file)
+    output_file = "utilisateurs.txt"
+    get_all_user(input_file, output_file)
+    write_suspect(input_file, "warning.txt", "suspect.txt")
